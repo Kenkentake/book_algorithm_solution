c1ec7e79dfdbe0f0d38e00459441afb166e4f7b5
Kenkentake <keeeentake0908@gmail.com>
2023-03-21 18:32:25 +0900

    add chap5's code

---
+++ chap5/levenshtein_distance.py
@@ -0,0 +1,30 @@
+from typing import TypeVar
+
+
+T = TypeVar("T")
+
+def chmin(a: T, b: T) -> int:
+    if a > b:
+        return b
+    else:
+        return a
+
+S = str(input())
+T = str(input())
+
+dp = [[1 << 29] * (len(T) + 1) for _ in range(len(S) + 1)]
+dp[0][0] = 0
+
+for i in range(len(S) + 1):
+    for j in range(len(T) + 1):
+        if i > 0 and j > 0:
+            if S[i - 1] == T[j - 1]:
+                dp[i][j] = chmin(dp[i][j], dp[i - 1][j - 1])
+            else:
+                dp[i][j] = chmin(dp[i][j], dp[i - 1][j - 1] + 1)
+        if i > 0:
+            dp[i][j] = chmin(dp[i][j], dp[i - 1][j] + 1)
+        if j > 0:
+            dp[i][j] = chmin(dp[i][j], dp[i][j - 1] + 1)
+
+print(dp[len(S)][len(T)])
