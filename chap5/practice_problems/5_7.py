82c40a5763d861cfa11a9fb72c638e366be25f0f
Kenkentake <keeeentake0908@gmail.com>
2023-03-25 22:15:30 +0900

    add practice_problems

---
+++ chap5/practice_problems/5_7.py
@@ -0,0 +1,32 @@
+from typing import TypeVar
+
+T = TypeVar("T")
+s = str(input())
+t = str(input())
+N = len(s)
+M = len(t)
+
+def chmax(a: T, b: T) -> T:
+    if a > b:
+        return a
+    else:
+        return b
+
+dp = [[0] * (M + 1) for _ in range(N + 1)]
+dp[0][0] = 0
+
+for i in range(N + 1):
+    for j in range(M + 1):
+        if i > 0 and j > 0:
+            if s[i - 1] == t[j - 1]:
+                dp[i][j] = chmax(dp[i][j], dp[i - 1][j - 1] + 1)
+            else:
+                dp[i][j] = chmax(dp[i][j], dp[i - 1][j - 1])
+
+        if i > 0:
+            dp[i][j] = chmax(dp[i][j], dp[i - 1][j])
+            
+        if j > 0:
+            dp[i][j] = chmax(dp[i][j], dp[i][j - 1])
+
+print(dp[N][M])
