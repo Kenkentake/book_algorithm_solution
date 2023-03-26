7895c72da418f5910ff4e6a0bc26a88df2e03567
Kenkentake <keeeentake0908@gmail.com>
2023-03-26 14:26:27 +0900

    add generalized binary search

---
+++ chap6/generalized_binary_search.py
@@ -0,0 +1,11 @@
+def P(x: int) -> bool:
+    pass
+
+def binary_search():
+    while ridge - left > 1:
+        mid = left + (right - left) // 2
+        if P(mid):
+            right = mid
+        else:
+            left = mid
+    return right
